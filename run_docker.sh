0e37c2617d03be36feed91c3bca4de01ea8f442a
EC2 Default User <ec2-user@ip-172-31-26-116.ec2.internal>
2022-10-17 20:23:00 +0000

    Tasks 1,2,3 completed

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,13 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag=proj04-app .
 
 # Step 2: 
 # List docker images
+##docker image ls
+docker image ls --filter=reference='proj04-app'
 
 # Step 3: 
 # Run flask app
+docker run -p 8000:80 proj04-app
