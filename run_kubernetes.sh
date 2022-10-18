8495bd3153ad8c72a2e0c08860b19d1a5c5549c7
Dan Wyllie <danwyllie@hotmail.com>
2022-10-18 14:30:19 +0100

    Tasks 4,5,6 completed

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,23 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=danwyllie/proj04-app
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run proj04-app --image=$dockerpath --port=80 --labels app=proj04-app
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
+until kubectl get pods | grep -q "Running";
+do
+    echo "Waiting for pod to start.."
+    sleep 2;
+done
+kubectl get pods
+#until kubectl get pods | grep -m 1 "Running"; do ; sleep 1 ; done
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward proj04-app 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=danwyllie/proj04-app
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag proj04-app $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
